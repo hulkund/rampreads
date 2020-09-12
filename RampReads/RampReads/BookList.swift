a0a17fef7c5f3c497acfdff59c8f1c061fbce035
Neha <nhulkund@mit.edu>
2020-09-11 21:10:44 -0700

    book stuff

---
+++ RampReads/RampReads/BookList.swift
@@ -0,0 +1,24 @@
+//
+//  BookList.swift
+//  RampReads
+//
+//  Created by Neha H on 9/11/20.
+//  Copyright © 2020 Shobhita Sundaram. All rights reserved.
+//
+
+import SwiftUI
+
+struct BookList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
